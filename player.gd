84471300a0d6475671303d97860688c82a7bfd5b
xturnalgamer <77756980+william-can@users.noreply.github.com>
2023-10-19 11:04:50 -0500

    end video 2

--- player.gd
+++ player.gd
@@ -6,17 +6,18 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	else:
+		is_jumping = false
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		is_jumping = true
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -24,5 +25,15 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	update_animation(direction)
 	move_and_slide()
+
+func update_animation(direction):
+	if is_jumping:
+		animated_sprite_2d.flip_h = direction < 0
+		animated_sprite_2d.play("jump")
+	elif direction != 0:
+		animated_sprite_2d.flip_h = direction < 0
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
