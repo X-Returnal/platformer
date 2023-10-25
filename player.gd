b13fa68c37fc9d3c8c7989b217705de60e38a64e
xturnalgamer <77756980+william-can@users.noreply.github.com>
2023-10-25 11:10:18 -0500

    end video 5

--- player.gd
+++ player.gd
@@ -1,16 +1,19 @@
 extends CharacterBody2D
 var is_jumping = false
-
+var is_dying = false
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var death_timer = $Death_Timer
 func _ready():
 	add_to_group("Player")
-
+	death_timer.connect("timeout",Callable(self, "on_DeathTimer_timeout"))
 func _physics_process(delta):
+	if is_dying:
+		return
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -31,6 +34,8 @@ func _physics_process(delta):
 	move_and_slide()
 
 func update_animation(direction):
+	if is_dying:
+		return
 	if is_jumping:
 		animated_sprite_2d.flip_h = direction < 0
 		animated_sprite_2d.play("jump")
@@ -39,3 +44,27 @@ func update_animation(direction):
 		animated_sprite_2d.play("run")
 	else:
 		animated_sprite_2d.play("idle")
+
+
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("Enemy") and body.is_alive:
+		die()
+func die():
+	if is_dying:
+		return
+	is_dying = true
+	animated_sprite_2d.play("die")
+	await die_animate()
+	get_tree().reload_current_scene()
+func die_animate():
+	var start_position = position
+	var up_position = start_position + Vector2(0,-100)
+	var down_position = start_position + Vector2(0,600)
+	while position.y > up_position.y:
+		position.y -= 4
+		await get_tree().create_timer(0.01).timeout
+	while position.y < down_position.y:
+		position.y += 4
+		await get_tree().create_timer(0.01).timeout
+func on_DeathTimer_timeout():
+	get_tree().reload_current_scene()
--- toad.gd
+++ toad.gd
@@ -2,12 +2,13 @@ extends CharacterBody2D
 
 
 const SPEED = 25.0
-
+var is_alive = true
 @onready var animated_sprite_2d = $AnimatedSprite2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
+func _ready():
+	add_to_group("Enemy")
 func _physics_process(delta):
 	#gravity
 	velocity.y += gravity * delta
@@ -16,7 +17,7 @@ func _physics_process(delta):
 	velocity.x = -SPEED
 
 	move_and_slide()
-func update_animation(direction):
+func update_animation(_direction):
 
 
 		animated_sprite_2d.play("hop")
@@ -24,4 +25,5 @@ func update_animation(direction):
 
 func _on_hitbox_body_entered(body):
 	if body.is_in_group("Player"):
+		is_alive= false
 		queue_free()
