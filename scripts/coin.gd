2a678ab7d66c2542506bc2d6bbb1bcbd59602d7b
xturnalgamer <77756980+william-can@users.noreply.github.com>
2023-10-27 10:58:01 -0500

    end video 8

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,8 @@
 extends Sprite2D
-
+var is_collected = false
 func _on_interaction_body_entered(body):
-	if body.is_in_group("Player"):
-		queue_free()
+	if body.is_in_group("Player") and not is_collected:
+		is_collected = true
+		Global.total_coins+=1
+		$interaction/AudioStreamPlayer2D.play()
+		hide()
