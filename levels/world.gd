7e795f70f1e75d32b895cd5af8fef91b01ab473a
xturnalgamer <77756980+william-can@users.noreply.github.com>
2023-10-30 11:03:54 -0500

    end video 9

--- levels/world.gd
+++ levels/world.gd
@@ -6,12 +6,18 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func _on_interaction_body_entered(body):
 	
 	if body.is_in_group("Player"):
 		body.die()
+
+
+func _on_transition_body_entered(body):
+	if body.is_in_group("Player"):
+		if Global.level == 0:
+			Global.level += 1
+			get_tree().change_scene_to_file("res://levels/transition_down.tscn")
+		elif Global.level == 1:
+			Global.level += 1
+			get_tree().change_scene_to_file("res://levels/world_2.tscn")
